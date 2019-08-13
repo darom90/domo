a540f52167384d5d8e3a964553d4cd10ad2289da
darom.chhuon <darom.chhuon@PNC.LAN>
2019-08-13 09:57:06 +0700

    message

---
+++ js/darom.js
@@ -0,0 +1,8 @@
+
+
+function changecolor(){
+    let container = document.getElementById("container");
+    let color = document.getElementById("colors").value;
+   container.style.background = color;
+    
+}
